fc78bab2b2878efb68ec09a18b4cdc60e76d98a0
brosea-bbcag <brosea@bbcag.ch>
2021-03-31 11:55:34 +0200

    Added labels to input
    Added a label to input fields

--- templates/calendar/index.php
+++ templates/calendar/index.php
@@ -301,7 +301,8 @@
             <a href="/calendar/last" class="btn btn-secondary px-5 mr-3">
                 Last
             </a>
-            <input type="date" class="input-week-start" value="<?=$startDate->format('Y-m-d')?>">
+            <label for="input-week-start-desktop"></label>
+            <input type="date" id="input-week-start-desktop" class="input-week-start" value="<?=$startDate->format('Y-m-d')?>">
             <span class="ml-2 mr-2">-</span>
             <span class="week-end">
                 <?=$endDate->format('d.m.Y')?>
@@ -345,7 +346,8 @@
         </button>
     </div>
     <div class="row d-flex justify-content-center mb-1">
-        <input type="date" class="input-week-start" value="<?=$startDate->format('Y-m-d')?>">
+        <label for="input-week-start-mobile"></label>
+        <input type="date" id="input-week-start-mobile" class="input-week-start" value="<?=$startDate->format('Y-m-d')?>">
         <span class="ml-2 mr-2">-</span>
         <span class="week-end">
             <?=$endDate->format('d.m.Y')?>
