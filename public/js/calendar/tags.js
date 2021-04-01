748dda855d70f17a2151f4218025f9385891de3d
von Wattenwyl Marino <bvonwm@bbcag.ch>
2021-04-01 11:55:43 +0200

    Fixed bug where shared email is sent twice

--- public/js/calendar/tags.js
+++ public/js/calendar/tags.js
@@ -119,10 +119,6 @@ function addTagToListAndSend(tagName, tagColor) {
                     "<span class=\"align-middle\">" + object.name + "</span>" +
                     "</div>"
             }
-            
-            // The checkboxes for the tags are now all disabled because we've edited the DOM
-            // This is why we reload the data from the server
-            requestAndFillInAppointmentData()
         }
     }
     request.send(data)
