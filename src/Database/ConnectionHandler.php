ff9e946fcd05f69fd24634d61cca4f71b307006a
Roser Alain <brosea@bbcag.ch>
2021-04-01 09:18:58 +0200

    Removed unnecessary pictures and comments
    Removed pictures which are never used or comments which are self-explanatory

--- src/Database/ConnectionHandler.php
+++ src/Database/ConnectionHandler.php
@@ -6,25 +6,8 @@ use App\Exception\DatabaseConnectionException;
 use MySQLi;
 use RuntimeException;
 
-/**
- * Der ConnectionHandler ist dafür zuständig, allen Repositories ein und die
- * selbe Verbindung auf die Datenbank zur Verfügung zu stellen.
- *
- * Bevor die Verbindung verwendet werden kann, muss die Datei config.php
- * entsprechend der Beschrebing in der Datei config.example.php erstellt werden.
- * Danach kann die Verbindung in den repositories folgendermassen aufgerufen
- * werden.
- *
- *   require_once 'ConnectionHandler.php';
- *   // ...
- *   $connection = ConnectionHandler::getConnection();
- */
 class ConnectionHandler {
-    /**
-     * Beim ersten Aufruf der getConnection Methode wird hier die Verbindung für
-     * die weiteren Aufrufe dieser Methode zwischengespeichert. Dadurch muss
-     * nicht für jedes Query eine neue Verbindung geöffnet werden.
-     */
+    
     private static $connection = null;
 
     /**
@@ -33,21 +16,9 @@ class ConnectionHandler {
      * Instanz existiert. Dies wird erreicht, indem der Konstruktor private ist
      * und die Methode getInstance die Instanziierung verwaltet.
      */
-    private function __construct() {
-        // Privater Konstruktor um die Verwendung von getInstance zu erzwingen.
-    }
-
-    /**
-     * Prüft ob bereits eine Verbindung auf die Datenbank existiert,
-     * initialisiert diese ansonsten und gibt sie dann zurück.
-     *
-     * @return Die mySQLi Verbindung, welche für den Zugriff aud die Datenbank
-     *             verwendet werden kann
-     * @throws Exception|DatabaseConnectionException wenn der Verbindungsaufbau schiefgegeangen ist
-     *
-     */
+    private function __construct() {}
+    
     public static function getConnection() {
-        // Prüfen ob bereits eine Verbindung existiert
         if (null === self::$connection) {
             $configFile = '../config.php';
 
@@ -61,7 +32,6 @@ class ConnectionHandler {
             $password = $config['database']['password'];
             $database = $config['database']['database'];
 
-            // Verbindung initialisieren
             self::$connection = new MySQLi($host, $username, $password, $database);
             if (self::$connection->connect_error) {
                 $error = self::$connection->connect_error;
@@ -72,7 +42,6 @@ class ConnectionHandler {
             self::$connection->set_charset('utf8');
         }
 
-        // Verbindung zurückgeben
         return self::$connection;
     }
 
